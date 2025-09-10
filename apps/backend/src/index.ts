68fbc656a03f134d5d9bd89c9e9560ae3db09484
Michael Hadley <michaelwesthadley@gmail.com>
2025-09-10 16:12:18 -0500

    Fix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.get("/", (_, res) => {
+  res.send("hello");
 });
 
 app.listen(port, () => {
